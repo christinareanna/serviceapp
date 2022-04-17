1bc94d7d582deb1f9fda6669641eefc69e2dd28a
Christina Knapp <christina.reanna.k@gmail.com>
2022-04-17 03:27:09 -0500

    changed to postgres, adjust register and login

---
+++ backend/index.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const app = express()
+const database = require("./database_connection")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const SECRET = "any_secret_you_want_to_use"
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get("/", (request, response, next) => {
+    response.sendStatus(200)
+})
+app.listen(9000)
+
+app.post("/users", (request, response, next) => {
+    bcrypt.hash(request.body.password, 10)
+        .then(hashedPassword => {
+            return database("user").insert({
+                email: request.body.email,
+                password_digest: hashedPassword
+            })
+                .returning(["id", "email"])
+                .then(users => {
+                    response.json(users[0])
+                })
+                .catch(error => next(error))
+        })
+})
+
+app.get("/users", (request, response, next) => {
+    database("user")
+        .then(users => {
+            response.json(users)
+        })
+})
+
+app.post("/login", (request, response, next) => {
+    database("user")
+        .where({ email: request.body.email })
+        .first()
+        .then(user => {
+            if (!user) {
+                response.status(401).json({
+                    error: "No email by that user."
+                })
+            } else {
+                return bcrypt
+                    .compare(request.body.password, user.password_digest)
+                    .then(isAuthenticated => {
+                        if (!isAuthenticated) {
+                            response.status(401).json({
+                                error: "Unauthorized Access!"
+                            })
+                        } else {
+                            return jwt.sign(user, SECRET, (error, token) => {
+                                response.status(200).json({ token })
+                            })
+                        }
+                    })
+            }
+        })
+})
+
+
+app.get("/verify", (request, response, next) => {
+    const token = request.headers.authorization.split(" ")[1]
+    jwt.verify(token, SECRET, (error, decodedToken) => {
+        if (error) {
+            response.status(401).json({
+                message: "Unauthorized Access!"
+            })
+        } else {
+            response.status(200).json({
+                id: decodedToken.id,
+                email: decodedToken.email
+            })
+        }
+    })
+})
--- components/UserRegistration.js
+++ components/UserRegistration.js
@@ -61,7 +61,6 @@
 import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import styles from './styles';
-import { firebase } from '../src/firebase/config';
 
 function RegistrationScreen({ navigation }) {
     const [fullName, setFullName] = useState('')
@@ -71,38 +70,11 @@ function RegistrationScreen({ navigation }) {
 
     const onFooterLinkPress = () => {
         navigation.navigate('Login')
+        // come back and adjust?
     }
 
     const onRegisterPress = () => {
-        // navigation.navigate('Register')
-        if (password !== confirmPassword) {
-            alert("Passwords don't match.")
-            return
-        }
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then((response) => {
-                const uid = response.user.uid
-                const data = {
-                    id: uid,
-                    email,
-                    fullName,
-                };
-                const usersRef = firebase.firestore().collection('users')
-                usersRef
-                    .doc(uid)
-                    .set(data)
-                    .then(() => {
-                        navigation.navigate('Home', { user: data })
-                    })
-                    .catch((error) => {
-                        alert(error)
-                    });
-            })
-            .catch((error) => {
-                alert(error)
-            });
+        // come back to make register possible 
     }
 
     return (
